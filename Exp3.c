a0808846fda6829d41d49b2f747fc9266a627e6a
johnqye <1534763483@qq.com>
2021-12-03 21:54:35 +0800

    Removed executable perm from source

--- Exp3.c
+++ Exp3.c
@@ -14,7 +14,6 @@ void genCover(covStore* this);
 void freeCov(covStore* this);
 void printCov(const covStore* this);
 
-
 int main(int argc, char** argv) {
     if (argc < 2)
         return printf("Usage: %s NUMBERS", argv[0]);
